9983ff1393fad8d191e913f9ab0379bc538f02fc
Daniel Moulton <dan.moulton@btinternet.com>
2025-12-13 12:12:39 +0000

    Day 6 Part 2
    Horribly done solution but beats nothign

--- solutions/day6.go
+++ solutions/day6.go
@@ -30,6 +30,8 @@ func main() {
 	result1 := part1(allNumbers, operators)
 	fmt.Printf("Part 1 Result: %d\n", result1)
 
+	result2 := part2(inputs)
+	fmt.Printf("Part 2 Result: %d\n", result2)
 }
 
 func part1(allNumbers [][]int, operators []rune) int {
@@ -39,7 +41,6 @@ func part1(allNumbers [][]int, operators []rune) int {
 	for i:=0; i<len(operators); i++ {
 		var nums []int
 		for j:=0; j<len(allNumbers); j++ {
-			// fmt.Printf("allNumbers[%d][%d]\n", j, i)
 			nums = append(nums, allNumbers[j][i])
 		}
 		var result int
@@ -58,5 +59,66 @@ func part1(allNumbers [][]int, operators []rune) int {
 		total += result
 	}
 	return total
+}
 
+func part2(inputs []string) int {
+	total := 0
+	maxLineLength := 0
+	for _, line := range inputs {
+		if len(line) > maxLineLength {
+			maxLineLength = len(line)
+		}
+	}
+
+	var numsBlock []int
+	var operator rune
+	for i := maxLineLength - 1; i >= 0; i-- {
+		nums := ""
+		if isEmptyColumn(inputs, i) {
+			// reset nums and operator
+			numsBlock = numsBlock[:0]
+			operator = 0
+			continue
+		}
+		for j := 0; j < len(inputs); j++ {
+			if i < len(inputs[j]) {
+				if inputs[j][i] >= '0' && inputs[j][i] <= '9' {
+					nums = nums + string(inputs[j][i])
+				} else if inputs[j][i] == '+' || inputs[j][i] == '*' {
+					operator = rune(inputs[j][i])
+				}
+			}
+		}
+		if nums != ""{
+			var colNum int
+			fmt.Sscanf(nums, "%d", &colNum)
+			numsBlock = append(numsBlock, colNum)
+		}
+		if operator != 0 {
+			switch operator {
+			case '+':
+				for _, n := range numsBlock {
+					total += n
+				}
+			case '*':
+				prod := 1
+				for _, n := range numsBlock {
+					prod *= n
+				}
+				total += prod
+			}
+		}
+	}
+	return total
+}
+
+func isEmptyColumn(inputs []string, col int) bool {
+	for _, line := range inputs {
+		if col < len(line) {
+			if line[col] != ' ' {
+				return false
+			}
+		}
+	}
+	return true
 }
